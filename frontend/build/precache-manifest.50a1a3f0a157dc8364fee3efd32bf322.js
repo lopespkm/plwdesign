327eef49b0a98491a6d12bce3bb2d2a68f2ac966
lopespkm <lopesgamersjrr@hotmail.com>
2024-05-21 14:54:50 -0300

    PLW Design

---
+++ frontend/build/precache-manifest.50a1a3f0a157dc8364fee3efd32bf322.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b35c63d7d32fe6fb5962c2de5b113d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2620b39e13e20167af01",
+    "url": "/static/css/2.406c6332.chunk.css"
+  },
+  {
+    "revision": "bf3d1da7c35e5ccd761e",
+    "url": "/static/css/main.40de1c4b.chunk.css"
+  },
+  {
+    "revision": "2620b39e13e20167af01",
+    "url": "/static/js/2.3e51ea32.chunk.js"
+  },
+  {
+    "revision": "719da88b2b4a1c7abe7d92d39bd84ca6",
+    "url": "/static/js/2.3e51ea32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffea0a23f49b3c637829",
+    "url": "/static/js/3.fb375776.chunk.js"
+  },
+  {
+    "revision": "d98f98f9156b1385986c1cdc16f69da5",
+    "url": "/static/js/3.fb375776.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf3d1da7c35e5ccd761e",
+    "url": "/static/js/main.e170fa81.chunk.js"
+  },
+  {
+    "revision": "cd844106a9b1076043cd",
+    "url": "/static/js/runtime-main.aa1411c1.js"
+  },
+  {
+    "revision": "2529c2597bdd5b6559aa1e276d74e930",
+    "url": "/static/media/chat_notify.2529c259.mp3"
+  },
+  {
+    "revision": "648831e851fff5177ff113e6a214a092",
+    "url": "/static/media/logo.648831e8.png"
+  },
+  {
+    "revision": "5c788b8d54d3ebf021114528116dfd77",
+    "url": "/static/media/n8n.5c788b8d.png"
+  },
+  {
+    "revision": "549edebaf5bafe89b539e694d4b1e26c",
+    "url": "/static/media/planilha.549edeba.xlsx"
+  },
+  {
+    "revision": "9fd5812c1c2c974cc29e53e138745809",
+    "url": "/static/media/sound.9fd5812c.mp3"
+  },
+  {
+    "revision": "8244f0423cb09d847ac99e7546cb9220",
+    "url": "/static/media/wa-background-dark.8244f042.png"
+  },
+  {
+    "revision": "2675a05168b3a218703c6d72bb336e02",
+    "url": "/static/media/wa-background.2675a051.png"
+  },
+  {
+    "revision": "e5374d73ff43538fdaa16a3f1a980f4b",
+    "url": "/static/media/webhook.e5374d73.png"
+  }
+]);
